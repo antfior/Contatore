42ccd3faee1f7ac3e115e1e6dabde7a74eb00df6
antfior <antfior93@gmail.com>
2018-03-26 15:29:08 +0200

    I set the ref of the layout

--- app/src/main/java/com/example/antfi/contatore/MainActivity.java
+++ app/src/main/java/com/example/antfi/contatore/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.antfi.contatore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //View del layout
 
+    private TextView  vMessaggio;
+    private Button    vAggiungi;
+    private Button    vDiminuisci;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vMessaggio = findViewById(R.id.textMessage);
+        vAggiungi = findViewById(R.id.BtnAdd);
+        vDiminuisci = findViewById(R.id.BtnMinus);
     }
 }
